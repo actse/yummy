15f4d6589150efb6cab8f0083c94395c1ce125a3
chotipong deemanop <chotipong.deemanop@gmail.com>
2023-08-16 14:16:17 +0700

    create homesafe.vue

---
+++ resources/js/Pages/HomeSafe.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-full min-h-screen bg-slate-200">
+      <div class="max-w-sm h-screen bg-slate-400 border">
+          <div class="w-full h-20 bg-white shadow-md"><h1>รายการ Order</h1></div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
