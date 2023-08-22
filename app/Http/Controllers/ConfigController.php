aa2be9f50cb01a6918813ef93b7e2a2b23ca098c
Bastsu <41604969+bastsu260741@users.noreply.github.com>
2023-08-22 17:54:17 +0700

    update config product

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -13,24 +13,43 @@ class ConfigController extends Controller
 {
     function addNewProduct(Request $request)
     {
-        $product_name = $request->input('product_name');
-        $product_detail = $request->input('product_detail');
-        $product_price = $request->input('product_price');
-        $type_product_id = $request->input('type_product_id');
+
         $product_image = $request->input('product_image');
-        $date_stamp = date('y-m-d h:i:s');
 
-        // $product_image_url = Storage::url($product_image->store('product_images', 'public'));
+        if ($product_image != '') {
 
-        return Product::insert([
-            'product_name' => $product_name,
-            'product_image' => $product_image,
-            'product_detail' => $product_detail,
-            'product_price' => $product_price,
-            'type_product_id' => $type_product_id,
-            'created_at' => $date_stamp,
-            'updated_at' => $date_stamp,
-        ]);
+            $product_name = $request->input('product_name');
+            $product_detail = $request->input('product_detail');
+            $product_price = $request->input('product_price');
+            $type_product_id = $request->input('type_product_id');
+            $product_image = $request->input('product_image');
+            $date_stamp = date('y-m-d h:i:s');
+
+            $imageName = time() . '_' . $product_image->getClientOriginalName();
+            $product_image->move(public_path('resources/imgs'), $imageName);
+
+            return Product::insert([
+                'product_name' => $product_name,
+                'product_image' => $product_image,
+                'product_detail' => $product_detail,
+                'product_price' => $product_price,
+                'type_product_id' => $type_product_id,
+                'created_at' => $date_stamp,
+                'updated_at' => $date_stamp,
+            ]);
+        }
+
+        return ('fail');
+
+        // return Product::insert([
+        //     'product_name' => $product_name,
+        //     'product_image' => $product_image,
+        //     'product_detail' => $product_detail,
+        //     'product_price' => $product_price,
+        //     'type_product_id' => $type_product_id,
+        //     'created_at' => $date_stamp,
+        //     'updated_at' => $date_stamp,
+        // ]);
     }
 
     function addTypeProductidtoPackage(Request $request)
